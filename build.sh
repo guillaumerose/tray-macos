c9c7366f3abdb709c32f24efd4fd407ebb4229e5
Guillaume Rose <gurose@redhat.com>
2020-09-25 12:28:02 +0200

    Use Swift PM instead of CocoaPods

--- build.sh
+++ build.sh
@@ -2,17 +2,12 @@
 
 BASE_DIR=$(pwd)
 BUILD_DIR=$BASE_DIR/out
-WORKSPACE=$BASE_DIR/CodeReady\ Containers.xcworkspace
-ARCHIVE_PATH=$BUILD_DIR/CodeReady_Containers.xcarchive
-EXPORT_OPTIONS=$BASE_DIR/CodeReady\ Containers/ExportOptions.plist
+PROJECT=$BASE_DIR/CodeReady\ Containers.xcodeproj
 SCHEME=CodeReady\ Containers
 
 if [ ! -f "$BUILD_DIR" ]; then
 	mkdir -p "$BUILD_DIR"
 fi
 
-# Build xcarchive
-xcodebuild -workspace "$WORKSPACE" -config Release -scheme "$SCHEME" -archivePath "$ARCHIVE_PATH" archive
+xcodebuild -project "$PROJECT" -config Release -scheme "$SCHEME" -derivedDataPath "$BUILD_DIR"
 
-# Build .app archive
-xcodebuild -exportArchive -archivePath "$ARCHIVE_PATH" -exportPath "$BUILD_DIR" -exportOptionsPlist "$EXPORT_OPTIONS"
